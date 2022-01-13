14c0766eab71e905ef4a104767369feadf965220
blockdoyle <blockdoyle@gmail.com>
2022-01-13 17:18:00 -0500

    Adds for loop to check each word to see if it's in blacklist.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import marisa_trie
+import marisa_trie, enchant
 
-def importBlacklist():
+def importBlacklist(): # imports blacklisted words from 'blacklist.txt'
     file = open("blacklist.txt")
     return file.read().splitlines()
 
-blacklistWords = importBlacklist()
+blacklistWords = importBlacklist() # calls importBlacklist function
 
-trie = marisa_trie.Trie(blacklistWords)
-print(trie.keys(""))
+trie = marisa_trie.Trie(blacklistWords) # takes the blacklistWords list and puts it into a marisa_trie trie
+
+sentence = "".lower()
+
+for word in sentence.split():
+    for entry in trie.keys(word):
+        if enchant.utils.levenshtein(word,entry) < 1:
+            print("banned word", word)
+    
+print(trie.keys(word))
