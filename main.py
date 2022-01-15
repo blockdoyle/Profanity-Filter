d23b64c4265a90a56b61f874f94524ca231c9d03
blockdoyle <blockdoyle@gmail.com>
2022-01-15 13:50:06 -0500

    updated to use difflib to get match percentage

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-import marisa_trie, enchant
+import marisa_trie, difflib
 
 def importBlacklist(): # imports blacklisted words from 'blacklist.txt'
     file = open("blacklist.txt")
     return file.read().splitlines()
 
+def convertToWhole(num): # converts float to integer
+    return int(num * 100)
+
+def getDifference(one,two): # gets the difference between 2 strings and returns as percentage
+    difference = difflib.SequenceMatcher(None,one,two).ratio()
+    return convertToWhole(difference)
+    
+def processString(txt): # Removes all punctuation from string
+    specialChars = "!,.'\""
+    for specialChars in specialChars:
+        txt = txt.replace(specialChars, "")
+    return txt
+
+def findBlacklistWord(testWord,trie): # compares a word against similarities in trie, and returns if 85% or mroe match
+    words = []
+    for word in trie:
+        if getDifference(testWord, word) > 85:
+            words.append(word)
+        else:
+            continue
+    return words
+
+#TESTING CODE BLOCK    
 blacklistWords = importBlacklist() # calls importBlacklist function
 
 blacklistTrie = marisa_trie.Trie(blacklistWords) # takes the blacklistWords list and puts it into a marisa_trie trie
 
-sentence = "".lower()
+msg = processString("You are such a fucking asshole, you know that? Go fuck yourself cunt.".lower()).split()
 
-for word in sentence.split(): # checks each word in sentence if it's in the trie
-    for entry in blacklistTrie.keys(word):
-        if enchant.utils.levenshtein(word,entry) < 1:
-            print("banned word", word)
-    
-print(blacklistTrie.keys(word))
+for word in msg:
+    print(findBlacklistWord(word, blacklistTrie.keys(word)))
